e54e81e7fa78f99d1f0fa0582f7277c8d7645052
TASagent <TASagent.Streams@gmail.com>
2024-12-01 07:58:23 -0600

    Adding Day 1

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,23 +1,54 @@
 ﻿const string inputFile = @"../../../../input01.txt";
 
-Console.WriteLine("Day 01 - ???");
+Console.WriteLine("Day 01 - Historian Hysteria");
 Console.WriteLine("Star 1");
 Console.WriteLine();
 
-//string[] lines = File.ReadAllLines(inputFile);
+string[] lines = File.ReadAllLines(inputFile);
 //string line = File.ReadAllText(inputFile);
 
-int value = 0;
+List<long> firstList = new List<long>();
+List<long> secondList = new List<long>();
 
-Console.WriteLine($"The answer is: {value}");
+
+
+foreach (string line in lines)
+{
+    string[] splitLine = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+    firstList.Add(long.Parse(splitLine[0]));
+    secondList.Add(long.Parse(splitLine[1]));
+}
+
+firstList.Sort();
+secondList.Sort();
+
+
+List<long> differenceList = firstList.Zip(secondList).Select(x => Math.Abs(x.First - x.Second)).ToList();
+
+
+Console.WriteLine($"The answer is: {differenceList.Sum()}");
 
 Console.WriteLine();
 Console.WriteLine("Star 2");
 Console.WriteLine();
 
-int value2 = 0;
+Dictionary<long, long> secondListCount = new Dictionary<long, long>();
 
-Console.WriteLine($"The answer is: {value2}");
+
+foreach(long value in secondList)
+{
+    if (!secondListCount.ContainsKey(value))
+    {
+        secondListCount.Add(value, 1);
+    }
+    else
+    {
+        secondListCount[value]++;
+    }
+}
+
+Console.WriteLine($"The answer is: {firstList.Where(secondListCount.ContainsKey).Select(x=> x * secondListCount[x]).Sum()}");
 
 Console.WriteLine();
 Console.ReadKey();
