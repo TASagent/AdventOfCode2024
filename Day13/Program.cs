1e3918713d08375df91f2d14413489ead18e1e5a
TASagent <TASagent.Streams@gmail.com>
2024-12-16 21:46:28 -0600

    Adding day 13

--- Day13/Program.cs
+++ Day13/Program.cs
@@ -1,13 +1,18 @@
-﻿const string inputFile = @"../../../../input13.txt";
+﻿using AoCTools;
+using System.Text.RegularExpressions;
 
-Console.WriteLine("Day 13 - ???");
+const string inputFile = @"../../../../input13.txt";
+
+Console.WriteLine("Day 13 - Claw Contraption");
 Console.WriteLine("Star 1");
 Console.WriteLine();
 
 //string[] lines = File.ReadAllLines(inputFile);
-//string line = File.ReadAllText(inputFile);
+List<string> chunks = File.ReadAllText(inputFile).Split("\r\n\r\n").ToList();
 
-int value = 0;
+List<ClawMachine> machines = chunks.Select(x=>new ClawMachine(x)).ToList();
+
+long value = machines.Select(x=>x.GetCost1()).Sum();
 
 Console.WriteLine($"The answer is: {value}");
 
@@ -15,9 +20,57 @@ Console.WriteLine();
 Console.WriteLine("Star 2");
 Console.WriteLine();
 
-int value2 = 0;
+long value2 = machines.Select(x => x.GetCost2()).Sum(); ;
 
 Console.WriteLine($"The answer is: {value2}");
 
 Console.WriteLine();
 Console.ReadKey();
+
+
+class ClawMachine
+{
+    public LongPoint2D ButtonA { get; }
+    public LongPoint2D ButtonB { get; }
+    public LongPoint2D Prize { get; }
+
+    public ClawMachine(string inputChunk)
+    {
+        string[] chunks = inputChunk.Split(new []{ '\n',  '+', ',', '=' });
+
+        ButtonA = (int.Parse(chunks[1]), int.Parse(chunks[3]));
+        ButtonB = (int.Parse(chunks[5]), int.Parse(chunks[7]));
+        Prize = (int.Parse(chunks[9]), int.Parse(chunks[11]));
+    }
+
+    public long GetCost1()
+    {
+        long determinantDenom = ButtonA.x * ButtonB.y - ButtonA.y * ButtonB.x;
+
+        long aPresses = (ButtonB.y * Prize.x - ButtonB.x * Prize.y) / determinantDenom;
+        long bPresses = (-ButtonA.y * Prize.x + ButtonA.x * Prize.y) / determinantDenom;
+
+        if (aPresses * ButtonA + bPresses * ButtonB != Prize)
+        {
+            return 0;
+        }
+
+        return 3 * aPresses + bPresses;
+    }
+
+    public long GetCost2()
+    {
+        LongPoint2D NewPrize = Prize + (10000000000000, 10000000000000);
+        long determinantDenom = ButtonA.x * ButtonB.y - ButtonA.y * ButtonB.x;
+
+        long aPresses = (ButtonB.y * NewPrize.x - ButtonB.x * NewPrize.y) / determinantDenom;
+        long bPresses = (-ButtonA.y * NewPrize.x + ButtonA.x * NewPrize.y) / determinantDenom;
+
+        if (aPresses * ButtonA + bPresses * ButtonB != NewPrize)
+        {
+            return 0;
+        }
+
+        return 3 * aPresses + bPresses;
+    }
+}
--- Day14/Program.cs
+++ Day14/Program.cs
@@ -1,10 +1,12 @@
-﻿const string inputFile = @"../../../../input14.txt";
+﻿using AoCTools;
 
-Console.WriteLine("Day 14 - ???");
+const string inputFile = @"../../../../input14.txt";
+
+Console.WriteLine("Day 14 - Restroom Redoubt");
 Console.WriteLine("Star 1");
 Console.WriteLine();
 
-//string[] lines = File.ReadAllLines(inputFile);
+string[] lines = File.ReadAllLines(inputFile);
 //string line = File.ReadAllText(inputFile);
 
 int value = 0;
@@ -21,3 +23,23 @@ Console.WriteLine($"The answer is: {value2}");
 
 Console.WriteLine();
 Console.ReadKey();
+
+
+class Robot
+{
+    Point2D Position { get; }
+    Point2D Velocity { get; }
+
+    public Robot(string line)
+    {
+        string[] splitLine = line.Split(new[] { ',', '=', ' ' });
+
+        Position = new Point2D(int.Parse(splitLine[1]),  int.Parse(splitLine[2]));
+        Velocity = new Point2D(int.Parse(splitLine[4]),  int.Parse(splitLine[5]));
+    }
+
+    Point2D GetPositionAfter(int time)
+    {
+
+    }
+}
